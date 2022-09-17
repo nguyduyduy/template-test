c597b2dbbd93656daad42f4847eb1c70a2a092d3
Duy Nguyen <duymisc@gmail.com>
2022-09-17 12:44:49 -0500

    changes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('testing');
+// Get Quotes From API 
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from API quotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+let apiQuotes = [];
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here 
+    }
+
+    // On Load 
+
+}
+
+getQuotes();
